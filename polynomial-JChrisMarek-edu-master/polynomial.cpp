66e571652341ba92fd731220381b09c358ca9b01
JChrisMarek-edu <marekj2@spu.edu>
2025-04-08 19:42:19 -0700

    v1.2

--- polynomial-JChrisMarek-edu-master/polynomial.cpp
+++ polynomial-JChrisMarek-edu-master/polynomial.cpp
@@ -17,7 +17,7 @@ using std::setprecision;
  6. The rest  of the methods
 
 */
-Polynomial::Polynomial(size_t degree){
+Polynomial::Polynomial(size_t degree){  // Construtor
 	_degree = degree + 1;
     if(_degree > 0){
     	_coefficients = new float[_degree];
@@ -27,18 +27,18 @@ Polynomial::Polynomial(size_t degree){
     }
     _degree -= 1;
 };
-Polynomial::Polynomial(size_t degree, const float* coefficients){
+Polynomial::Polynomial(size_t degree, const float* coefficients){  // Constructor
 	_degree = degree;
     _coefficients = const_cast<float*>(coefficients);
 };
-Polynomial::Polynomial(const Polynomial& other){
+Polynomial::Polynomial(const Polynomial& other){  // Copy Constructor
 	_degree = other._degree;
     _coefficients = other._coefficients;
 };
-Polynomial::~Polynomial(){
+Polynomial::~Polynomial(){  // Destructor
 	delete[] _coefficients;
 }
-const Polynomial Polynomial::Sum(const Polynomial& rhs)const{
+const Polynomial Polynomial::Sum(const Polynomial& rhs)const{  // Summation of two Polynomials
   	float retValSize;
   	if(this->_degree >= rhs._degree){
   		retValSize = this->_degree;
@@ -55,7 +55,7 @@ const Polynomial Polynomial::Sum(const Polynomial& rhs)const{
   	return retVal;
 
 }
-const Polynomial Polynomial::Subtract(const Polynomial& rhs)const{
+const Polynomial Polynomial::Subtract(const Polynomial& rhs)const{  // Subtraction of two Polynomials
 	float retValSize;
   	if(this->_degree >= rhs._degree){
   		retValSize = this->_degree;
@@ -71,14 +71,14 @@ const Polynomial Polynomial::Subtract(const Polynomial& rhs)const{
   	}
   	return retVal;
 }
-const Polynomial Polynomial::Minus()const{
+const Polynomial Polynomial::Minus()const{  // Unary Minus aka sign flip
 	Polynomial retVal(this->_degree);
     for(size_t i = 0; i < this->_degree; i++){
     	retVal._coefficients[i] = -1 * this->_coefficients[i];
     }
 	return retVal;
 }
-const Polynomial Polynomial::Multiply(const Polynomial& rhs)const{
+const Polynomial Polynomial::Multiply(const Polynomial& rhs)const{  // Multiplication of two Polynomials
 	Polynomial retVal(this->_degree + rhs._degree);
     for(size_t i = 0; i <= this->_degree; i++){
     	for(size_t j = 0; j <= rhs._degree; j++){
@@ -87,31 +87,31 @@ const Polynomial Polynomial::Multiply(const Polynomial& rhs)const{
     }
   	return retVal;
 }
-const Polynomial Polynomial::Divide(const Polynomial& rhs)const{
+const Polynomial Polynomial::Divide(const Polynomial& rhs)const{  // Division of two Polynomials *EC*
     return Polynomial(0);
 }
-const Polynomial Polynomial::Divide(const Polynomial& rhs, Polynomial& remainder)const{
+const Polynomial Polynomial::Divide(const Polynomial& rhs, Polynomial& remainder)const{  // Division of two Polynomials *EC*
     remainder = Polynomial(0);
     return Polynomial(0);
 }
-const Polynomial Polynomial::Derive()const{
+const Polynomial Polynomial::Derive()const{  // Derivation of a single Polynomial
 	Polynomial retVal(this->_degree - 1);
     for(size_t i = 1; i < retVal._degree + 1; i++){
     	 retVal._coefficients[i - 1] = this->_coefficients[i] * i;
     }
   	return retVal;
 }
-float Polynomial::Evaluate(float x)const{
+float Polynomial::Evaluate(float x)const{  // Evaluation of a single Polynomial
 	float result = _coefficients[0];
     for(size_t i = 1; i <= this->_degree; i++){
     	result += _coefficients[i] * pow(x, i);
     }
   	return result;
 }
-float Polynomial::Integrate(float start, float end)const{
+float Polynomial::Integrate(float start, float end)const{  // Integration of a single Polynomial *EC*
     return 0.0;
 }
-const Polynomial& Polynomial::operator=(const Polynomial& rhs){
+const Polynomial& Polynomial::operator=(const Polynomial& rhs){  // Copy Assignment Operator
     if(this != &rhs){
     	delete[] _coefficients;
     	this->_coefficients = rhs._coefficients;
@@ -119,7 +119,7 @@ const Polynomial& Polynomial::operator=(const Polynomial& rhs){
     }
     return *this;
 }
-string Polynomial::ToString()const{
+string Polynomial::ToString()const{  // Converts to string *Provided Do not Touch*
     stringstream ss;
     for (size_t i = _degree; i > 0; i--) {
         ss << showpos << fixed << setprecision(2) << _coefficients[i] << "x^" << i << " ";
@@ -127,7 +127,7 @@ string Polynomial::ToString()const{
     ss << showpos << fixed << setprecision(2) << _coefficients[0];
     return ss.str();
 }
-bool Polynomial::Equals(const Polynomial& rhs)const{
+bool Polynomial::Equals(const Polynomial& rhs)const{ // Checks if two Polynomials are the same
 	if(this->_degree != rhs._degree){
     	return false;
     } else {
@@ -139,14 +139,14 @@ bool Polynomial::Equals(const Polynomial& rhs)const{
     }
     return true;
 }
-ostream& Polynomial::Write(ostream& output)const{
+ostream& Polynomial::Write(ostream& output)const{  // Writes to a stream *Provided Do not Touch*
     output << _degree << " ";
     for (size_t i = 0; i < _degree + 1; i++) {
         output << _coefficients[i] << " ";
     }
     return output;
 }
-istream& Polynomial::Read(istream& input){
+istream& Polynomial::Read(istream& input){  // Reads from a stream *Provided Do not Touch*
     size_t degree;
     input >> degree;
     if (input.fail()){
